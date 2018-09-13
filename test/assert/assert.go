1a66402d051f7cf4055c5db70302f2715d776d51
Markus <markus@greenrobot>
2018-09-13 12:24:58 +0200

    assert.go shows stack, etc.

--- test/assert/assert.go
+++ test/assert/assert.go
@@ -1,15 +1,67 @@
 package assert
 
-import "strconv"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"runtime/debug"
+	"testing"
+)
 
-func EqString(expected string, actual string) {
+func EqString(t *testing.T, expected string, actual string) {
 	if expected != actual {
-		panic("Expected \"" + expected + "\", but got \"" + actual + "\"")
+		debug.PrintStack()
+		Failf(t, "Expected \""+expected+"\", but got \""+actual+"\"")
 	}
 }
 
-func EqInt(expected int, actual int) {
+func EqInt(t *testing.T, expected int, actual int) {
 	if expected != actual {
-		panic("Expected \"" + strconv.Itoa(expected) + "\", but got \"" + strconv.Itoa(actual) + "\"")
+		debug.PrintStack()
+		Failf(t, "Expected %v, but got %v", expected, actual)
 	}
+}
+
+func NoErr(t *testing.T, err error) {
+	if err != nil {
+		Failf(t, "Unexpected error occurred: %v", err)
+	}
+}
+
+func Failf(t *testing.T, format string, args ...interface{}) {
+	Fail(t, fmt.Sprintf(format, args...))
+}
+
+func Fail(t *testing.T, text string) {
+	firstLocation := ""
+	stack_count := 0
+	stack_string := ""
+	for idx := 1; ; idx++ {
+		_, file, line, ok := runtime.Caller(idx)
+		if !ok {
+			break
+		}
+		_, filename := filepath.Split(file)
+		if filename == "assert.go" {
+			continue
+		}
+		if filename == "testing.go" {
+			break
+		}
+		location := fmt.Sprintf("%v:%v", filename, line)
+		if firstLocation == "" {
+			firstLocation = location
+		}
+		stack_string += fmt.Sprintf("    %v\n", location)
+		stack_count++
+	}
+	if stack_count > 1 {
+		fmt.Println("Full stack for [" + text + "]:\n" + stack_string)
+
+	}
+
+	bufio.NewWriter(os.Stdout).Flush()
+	t.Fatal(text, "\nat", firstLocation)
 }
--- test/query_test.go
+++ test/query_test.go
@@ -14,26 +14,19 @@ func TestQueryBuilder(t *testing.T) {
 	box.RemoveAll()
 
 	qb, err := objectBox.Query(1)
-	if err != nil {
-		t.Fatalf("Could not build QB: %v", err)
-	}
+	assert.NoErr(t, err)
 	query, err := qb.BuildAndDestroy()
-	if err != nil {
-		t.Fatalf("Could not build query: %v", err)
-	}
+	assert.NoErr(t, err)
 	defer query.Destroy()
 
 	objectBox.RunWithCursor(1, true, func(cursor *objectbox.Cursor) (err error) {
 		bytesArray, err := query.FindBytes(cursor)
-		if err != nil {
-			t.Fatalf("Could not run query: %v", err)
-		}
-		assert.EqInt(0, len(bytesArray.BytesArray))
+		assert.NoErr(t, err)
+		assert.EqInt(t, 0, len(bytesArray.BytesArray))
 
 		slice, err := query.Find(cursor)
-		if err != nil {
-			t.Fatalf("Could not run query: %v", err)
-		}
+		assert.NoErr(t, err)
+
 		// TODO should be empty slice instead of nil
 		if slice != nil && len(slice.([]object.Event)) != 0 {
 			t.Fatalf("unexpected size")
@@ -45,9 +38,7 @@ func TestQueryBuilder(t *testing.T) {
 		Device: "dev1",
 	}
 	id1, err := box.Put(&event)
-	if err != nil {
-		t.Fatalf("Could not put event: %v", err)
-	}
+	assert.NoErr(t, err)
 
 	event.Device = "dev2"
 	id2, err := box.Put(&event)
@@ -57,25 +48,21 @@ func TestQueryBuilder(t *testing.T) {
 
 	objectBox.RunWithCursor(1, true, func(cursor *objectbox.Cursor) (err error) {
 		bytesArray, err := query.FindBytes(cursor)
-		if err != nil {
-			t.Fatalf("Could not run query: %v", err)
-		}
-		assert.EqInt(2, len(bytesArray.BytesArray))
+		assert.NoErr(t, err)
+		assert.EqInt(t, 2, len(bytesArray.BytesArray))
 
 		slice, err := query.Find(cursor)
-		if err != nil {
-			t.Fatalf("Could not run query: %v", err)
-		}
+		assert.NoErr(t, err)
 		events := slice.([]object.Event)
 		if len(events) != 2 {
 			t.Fatalf("unexpected size")
 		}
 
-		assert.EqInt(int(id1), int(events[0].Id))
-		assert.EqString("dev1", events[0].Device)
+		assert.EqInt(t, int(id1), int(events[0].Id))
+		assert.EqString(t, "dev1", events[0].Device)
 
-		assert.EqInt(int(id2), int(events[1].Id))
-		assert.EqString("dev2", events[1].Device)
+		assert.EqInt(t, int(id2), int(events[1].Id))
+		assert.EqString(t, "dev2", events[1].Device)
 
 		return
 	})

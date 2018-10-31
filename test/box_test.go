4823a916736d12c7fcf322ff3782865c8d7fc531
Ivan <noreply+ivan@objectbox.io>
2018-10-31 14:05:03 +0100

    binding generator - WIP BoxForEntity draft

--- test/box_test.go
+++ test/box_test.go
@@ -1,9 +1,12 @@
 package objectbox_test
 
 import (
+	"fmt"
+	"testing"
+
+	"github.com/objectbox/objectbox-go/objectbox"
 	"github.com/objectbox/objectbox-go/test/model/iot"
 	. "github.com/objectbox/objectbox-go/test/model/iot/object"
-	"testing"
 )
 
 func TestAsync(t *testing.T) {
@@ -26,4 +29,56 @@ func TestAsync(t *testing.T) {
 	if objectId != eventRead.Id || event.Device != eventRead.Device {
 		t.Fatalf("Event data error: %v vs. %v", event, eventRead)
 	}
+}
+
+//go:generate objectbox-bindings
+type Person struct {
+	Id        uint64 `id`
+	FirstName string
+	LastName  string
+}
+
+func TestGeneratedBox(t *testing.T) {
+	ob := iot.CreateObjectBox()
+
+	box := BoxForPerson(ob)
+
+	// Create
+	id, _ := box.Put(&Person{
+		FirstName: "Joe",
+		LastName:  "Green",
+	})
+
+	person, _ := box.Get(id) // Read
+	person.LastName = "Black"
+	box.Put(person)    // Update
+	box.Remove(person) // Delete
+}
+
+// TODO generated code follows
+type PersonBox struct {
+	objectbox.Box
+	EntityId uint32
+}
+
+func BoxForPerson(ob *objectbox.ObjectBox) *PersonBox {
+	return &PersonBox{
+		EntityId: uint32(1),
+	}
+}
+
+func (box *PersonBox) Get(id uint64) (person *Person, err error) {
+	object, err := box.Box.Get(id)
+	if err != nil {
+		return nil, err
+	}
+	person, ok := object.(*Person)
+	if !ok {
+		return nil, fmt.Errorf("could not convert object to Person, invalid type")
+	}
+	return person, nil
+}
+
+func (box *PersonBox) Remove(person *Person) (err error) {
+	return box.Box.Remove(person.Id)
 }

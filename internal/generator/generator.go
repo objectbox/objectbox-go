98d6480a766cad1ef639965c80373d0750b5be49
Ivan <noreply+ivan@objectbox.io>
2018-10-31 10:40:25 +0100

    binding generator - rename cmd, write formatted bindings file

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -1,8 +1,13 @@
 package generator
 
 import (
+	"bufio"
+	"bytes"
+	"go/format"
 	"html/template"
+	"io/ioutil"
 	"os"
+	"path"
 	"strings"
 
 	// TODO check whether we can use this dependency
@@ -10,9 +15,9 @@ import (
 	"github.com/gobuffalo/packr"
 )
 
-func Process(filePath string) (err error) {
+func Process(sourceFile string) (err error) {
 	var f *file
-	if f, err = parseFile(filePath); err != nil {
+	if f, err = parseFile(sourceFile); err != nil {
 		return err
 	}
 
@@ -25,30 +30,61 @@ func Process(filePath string) (err error) {
 		return err
 	}
 
-	if err = generateBindingFile(binding); err != nil {
+	var bindingSource []byte
+	if bindingSource, err = generateBinding(binding); err != nil {
+		return err
+	}
+
+	var extension = path.Ext(sourceFile)
+	var bindingFile = sourceFile[0:len(sourceFile)-len(extension)] + "binding" + extension
+
+	if err = writeBindingFile(sourceFile, bindingFile, bindingSource); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func generateBindingFile(binding *Binding) (err error) {
+func generateBinding(binding *Binding) (data []byte, err error) {
 	// load the template for the binding
 	box := packr.NewBox("./templates")
 
 	var tplText string
 	if tplText, err = box.MustString("binding.tmpl"); err != nil {
-		return err
+		return nil, err
 	}
 
 	funcMap := template.FuncMap{
 		"StringTitle": strings.Title,
 	}
 
+	// prepare the template
 	tpl := template.Must(template.New("binding").Funcs(funcMap).Parse(tplText))
-	if err = tpl.Execute(os.Stdout, binding); err != nil { // TODO replace os.Stdout with file writer
+
+	var b bytes.Buffer
+	writer := bufio.NewWriter(&b)
+	if err = tpl.Execute(writer, binding); err != nil {
+		return nil, err
+	}
+
+	if err = writer.Flush(); err != nil {
+		return nil, err
+	}
+
+	// format the bindings source (same as gofmt)
+	return format.Source(b.Bytes())
+}
+
+func writeBindingFile(sourceFile string, bindingFile string, data []byte) (err error) {
+	var perm os.FileMode
+	// copy permissions either from the existing bindings file or from the source file
+	if info, _ := os.Stat(bindingFile); info != nil {
+		perm = info.Mode()
+	} else if info, err := os.Stat(sourceFile); info != nil {
+		perm = info.Mode()
+	} else {
 		return err
 	}
 
-	return nil
+	return ioutil.WriteFile(bindingFile, data, perm)
 }

8c45823f5d6c25ce09f6a9ee226c586b4d2379eb
Ivan Dlugos <noreply+ivan@objectbox.io>
2018-11-14 19:35:32 +0100

    tasks example - interactive console, docs, generated sources

---
+++ examples/tasks/internal/model/objectbox-model-info.js
@@ -0,0 +1,41 @@
+{
+  "Comment": [
+    "KEEP THIS FILE! Check it into a version control system (VCS) like git.",
+    "ObjectBox manages crucial IDs for your object model. See docs for details.",
+    "If you have VCS merge conflicts, you must resolve them according to ObjectBox docs."
+  ],
+  "Entities": [
+    {
+      "Id": "1:1306759095002958910",
+      "Name": "Task",
+      "LastPropertyId": "4:8083684673086871702",
+      "Properties": [
+        {
+          "Id": "1:2193439623591184445",
+          "Name": "Id",
+          "IndexId": null
+        },
+        {
+          "Id": "2:6177929178231325611",
+          "Name": "Text",
+          "IndexId": null
+        },
+        {
+          "Id": "3:9141374017424160113",
+          "Name": "DateCreated",
+          "IndexId": null
+        },
+        {
+          "Id": "4:8083684673086871702",
+          "Name": "DateFinished",
+          "IndexId": null
+        }
+      ]
+    }
+  ],
+  "LastEntityId": "1:1306759095002958910",
+  "LastIndexId": "",
+  "RetiredEntityUids": [],
+  "RetiredIndexUids": [],
+  "RetiredPropertyUids": []
+}
--- examples/tasks/main.go
+++ examples/tasks/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/objectbox/objectbox-go/objectbox"
@@ -10,34 +14,83 @@ import (
 )
 
 func main() {
-	//reader := bufio.NewReader(os.Stdin)
-
-	fmt.Println("Welcome to the ObjectBox tasks-list example")
-	fmt.Println()
-
 	// load objectbox
 	ob := initObjectBox()
-	//defer ob.Destroy()
+	defer ob.Destroy()
+
 	box := model.BoxForTask(ob)
-	//defer box.Destroy()
+	defer box.Destroy()
 
-	createTask(box, "Text")
-	printList(box)
-	setDone(box, 1)
-	printList(box)
+	runInteractiveShell(box)
+}
 
-	box.Destroy()
-	ob.Destroy()
+func runInteractiveShell(box *model.TaskBox) {
+	// our simple interactive shell
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Welcome to the ObjectBox tasks-list app example")
+	printHelp()
+
+	for {
+		fmt.Print("$ ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		//input = strings.TrimSuffix(input, "\n")
+		input = strings.TrimSpace(input)
+		args := strings.Fields(input)
+
+		switch strings.ToLower(args[0]) {
+		case "new":
+			createTask(box, strings.Join(args[1:], " "))
+		case "done":
+			if len(args) != 2 {
+				fmt.Fprintf(os.Stderr, "wrong number of arguments, expecting exactly one\n")
+			} else if id, err := strconv.ParseUint(args[1], 10, 64); err != nil {
+				fmt.Fprintf(os.Stderr, "could not parse ID: %s\n", err)
+			} else {
+				setDone(box, id)
+			}
+		case "ls":
+			if len(args) < 2 {
+				printList(box, false)
+			} else if args[1] == "-a" {
+				printList(box, true)
+			} else {
+				fmt.Fprintf(os.Stderr, "unknown argument %s\n", args[1])
+				fmt.Println()
+			}
+		case "exit":
+			return
+		case "help":
+			printHelp()
+		default:
+			fmt.Fprintf(os.Stderr, "unknown command %s\n", input)
+			printHelp()
+		}
+	}
 }
 
 func initObjectBox() *objectbox.ObjectBox {
-	builder := objectbox.NewObjectBoxBuilder().LastEntityId(1, 2351059844987470165)
+	// see generated internal/model/objectbox-model-info.js for the LastEntity ID & UID
+	builder := objectbox.NewObjectBoxBuilder().LastEntityId(1, 1306759095002958910)
 	builder.RegisterBinding(model.TaskBinding{})
 	objectBox, err := builder.Build()
 	if err != nil {
 		panic(err)
 	}
 	return objectBox
+}
+
+func printHelp() {
+	fmt.Println("Available commands are: ")
+	fmt.Println("    ls [-a]        list tasks - unfinished or all (-a flag)")
+	fmt.Println("    new Task text  create a new task with the text 'Task text'")
+	fmt.Println("    done ID        mark task with the given ID as done")
+	fmt.Println("    exit           close the program")
+	fmt.Println("    help           display this help")
 }
 
 func createTask(box *model.TaskBox, text string) {
@@ -47,19 +100,24 @@ func createTask(box *model.TaskBox, text string) {
 	}
 
 	if id, err := box.Put(task); err != nil {
-		fmt.Printf("could not create task: %s\n", err)
+		fmt.Fprintf(os.Stderr, "could not create task: %s\n", err)
 	} else {
 		task.Id = id
 		fmt.Printf("task ID %d successfully created\n", task.Id)
 	}
 }
 
-func printList(box *model.TaskBox) {
+func printList(box *model.TaskBox, all bool) {
 	if list, err := box.GetAll(); err != nil {
-		fmt.Printf("could not list tasks: %s\n", err)
+		fmt.Fprintf(os.Stderr, "could not list tasks: %s\n", err)
 	} else {
 		fmt.Printf("%3s  %-29s  %-29s  %s\n", "ID", "Created", "Finished", "Text")
 		for _, task := range list {
+			if task.DateFinished != 0 && !all {
+				// skip finished tasks if we aren't printing everything
+				// NOTE you could use Queries to do this in the future but let's keep the example simple
+				continue
+			}
 			fmt.Printf("%3d  %-29s  %-29s  %s\n",
 				task.Id, fmtTime(task.DateCreated), fmtTime(task.DateFinished), task.Text)
 		}
@@ -68,11 +126,11 @@ func printList(box *model.TaskBox) {
 
 func setDone(box *model.TaskBox, id uint64) {
 	if task, err := box.Get(id); err != nil {
-		fmt.Printf("could not read task ID %d: %s\n", id, err)
+		fmt.Fprintf(os.Stderr, "could not read task ID %d: %s\n", id, err)
 	} else {
 		task.DateFinished = obNow()
 		if _, err := box.Put(task); err != nil {
-			fmt.Printf("could not update task ID %d: %s\n", id, err)
+			fmt.Fprintf(os.Stderr, "could not update task ID %d: %s\n", id, err)
 		} else {
 			fmt.Printf("task ID %d completed at %s\n", id, fmtTime(task.DateFinished))
 		}

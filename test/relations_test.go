1d5ded0c306aa0bbdf73de002c715227df097ce1
Ivan Dlugos <noreply+ivan@objectbox.io>
2019-01-29 17:07:12 +0100

    test many-to-many relations handling in insert & read

--- test/relations_test.go
+++ test/relations_test.go
@@ -17,6 +17,7 @@
 package objectbox_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/objectbox/objectbox-go/test/assert"
@@ -24,17 +25,25 @@ import (
 	"github.com/objectbox/objectbox-go/test/model"
 )
 
-func TestRelations(t *testing.T) {
+func TestRelationsInsert(t *testing.T) {
 	// run once for Put & once for PutAsync
 	for i := 0; i <= 1; i++ {
 		// starting the environment inside the for loop ensures the database is empty & IDs start from 0
 		var env = model.NewTestEnv(t)
 		var relBox = model.BoxForTestEntityRelated(env.ObjectBox)
+		var relValueBox = model.BoxForEntityByValue(env.ObjectBox)
 
 		var id uint64
 		var err error
 
-		var entity = &model.Entity{RelatedPtr: &model.TestEntityRelated{Name: "Ptr"}, Related: model.TestEntityRelated{Name: "Val"}}
+		// this object is used in many-to-many (slice) & one-to-many relations and is inserted only once
+		var relReused = &model.TestEntityRelated{Name: "Ptr"}
+		var entity = &model.Entity{
+			Related:         model.TestEntityRelated{Name: "Val"},
+			RelatedPtr:      relReused,
+			RelatedSlice:    []model.EntityByValue{{}, {}},
+			RelatedPtrSlice: []*model.TestEntityRelated{relReused, {Name: "New"}},
+		}
 
 		if i == 0 {
 			id, err = env.Box.Put(entity)
@@ -44,15 +53,29 @@ func TestRelations(t *testing.T) {
 		}
 
 		assert.NoErr(t, err)
-		assert.Eq(t, id, uint64(1))
-		assert.Eq(t, entity.Related.Id, uint64(1))
-		assert.Eq(t, entity.RelatedPtr.Id, uint64(2))
+		assert.Eq(t, uint64(1), id)
+		assert.Eq(t, uint64(1), entity.Related.Id)
+		assert.Eq(t, uint64(2), entity.RelatedPtr.Id)
+		assert.Eq(t, uint64(2), entity.RelatedPtr.Id)
+		assert.Eq(t, uint64(1), entity.RelatedSlice[0].Id)
+		assert.Eq(t, uint64(2), entity.RelatedSlice[1].Id)
+		assert.Eq(t, uint64(2), entity.RelatedPtrSlice[0].Id)
+		assert.Eq(t, uint64(3), entity.RelatedPtrSlice[1].Id)
 
 		// check that the relations are inserted and are the same as the ones in the entity
 		rels, err := relBox.GetAll()
 		assert.NoErr(t, err)
+		assert.Eq(t, 3, len(rels))
 		assert.Eq(t, *rels[0], entity.Related)
 		assert.Eq(t, *rels[1], *entity.RelatedPtr)
+		assert.Eq(t, *rels[1], *entity.RelatedPtrSlice[0])
+		assert.Eq(t, *rels[2], *entity.RelatedPtrSlice[1])
+
+		relsV, err := relValueBox.GetAll()
+		assert.NoErr(t, err)
+		assert.Eq(t, 2, len(relsV))
+		assert.Eq(t, relsV[0], entity.RelatedSlice[0])
+		assert.Eq(t, relsV[1], entity.RelatedSlice[1])
 
 		// try to read the entity and validate it's read correctly with relations assigned
 		entityRead, err := env.Box.Get(id)
@@ -60,12 +83,23 @@ func TestRelations(t *testing.T) {
 		assert.Eq(t, entity.Related, entityRead.Related)
 		assert.Eq(t, entity.RelatedPtr, entityRead.RelatedPtr)
 		assert.Eq(t, entity.RelatedPtr2, entityRead.RelatedPtr2)
+		assert.Eq(t, entity.RelatedSlice, entityRead.RelatedSlice)
+		assert.Eq(t, entity.RelatedPtrSlice, entityRead.RelatedPtrSlice)
 
-		// remove one relation and try to read the entity
+		// remove one target of each relation, read the entity and check everything looks as expected (relations are removed)
 		assert.NoErr(t, relBox.Remove(&entity.Related))
 		assert.NoErr(t, relBox.Remove(entity.RelatedPtr))
+		assert.NoErr(t, relValueBox.Remove(&entity.RelatedSlice[0]))
 		entityRead, err = env.Box.Get(id)
-		assert.Eq(t, entityRead.Related.Id, uint64(0))
-		assert.Eq(t, entityRead.RelatedPtr == nil, true)
+
+		fmt.Println(rels[0])
+		assert.Eq(t, uint64(0), entityRead.Related.Id)
+		assert.Eq(t, true, entityRead.RelatedPtr == nil)
+		assert.Eq(t, 1, len(entityRead.RelatedSlice))
+		assert.Eq(t, relsV[1], entityRead.RelatedSlice[0])
+		assert.Eq(t, 1, len(entityRead.RelatedPtrSlice))
+		assert.Eq(t, rels[2], entityRead.RelatedPtrSlice[0])
 	}
 }
+
+// TODO test update of a source entity while inserting/deleting relations

0be30f7b07446990b2ea2ea863b577bffdd31913
Ivan Dlugos <noreply+ivan@objectbox.io>
2019-04-18 15:02:09 +0200

    roll-back a code importer.For/ForCompiler change to keep compatibility with Go 1.11

--- internal/generator/ast.go
+++ internal/generator/ast.go
@@ -115,7 +115,9 @@ func (f *file) getType(expr ast.Expr) (types.Type, error) {
 		var conf = types.Config{
 			IgnoreFuncBodies:         true,
 			DisableUnusedImportCheck: true,
-			Importer:                 importer.ForCompiler(f.fileset, "source", nil),
+			// TODO switch to the new method (available since Go 1.12)
+			//  Importer:                 importer.ForCompiler(f.fileset, "source", nil),
+			Importer:                 importer.For("source", nil),
 		}
 		if _, err := conf.Check(f.dir, f.fileset, f.files, f.info); err != nil {
 			return nil, fmt.Errorf("error running type-check: %s", err)

b768e34725410b277977b177f0cbb34f4ea4af57
Ivan Dlugos <noreply+ivan@objectbox.io>
2019-07-12 15:10:58 +0200

    improve install script

--- install.sh
+++ install.sh
@@ -1,10 +1,43 @@
 #!/usr/bin/env bash
+set -eu
 
-mkdir objectboxlib && cd objectboxlib
-curl https://raw.githubusercontent.com/objectbox/objectbox-c/master/download.sh > download.sh
-bash download.sh
+goVersion=$(go version | cut -d' ' -f 3)
+goVersionMajor=$(echo "${goVersion}" | cut -d'.' -f1)
+goVersionMinor=$(echo "${goVersion}" | cut -d'.' -f2)
+goVersionPatch=$(echo "${goVersion}" | cut -d'.' -f3)
 
-go get github.com/objectbox/objectbox-go/...
-go install github.com/objectbox/objectbox-go/cmd/objectbox-gogen/
+if [ ! "${goVersionMajor}" == "go1" ]; then
+  echo "Unexpected Go major version ${goVersionMajor}, expecting Go 1.11.4+."
+  echo "You can proceed and let us know if you think we should extend the support to your version."
+elif [ "${goVersionMinor}" -lt "11" ]; then
+  echo "Invalid Go version ${goVersion}, at least 1.11.4 required"
+  exit 1
+elif [ "${goVersionMinor}" == "11" ] && [ "${goVersionPatch}" -lt "4" ]; then
+  echo "Invalid Go version ${goVersion}, at least 1.11.4 required"
+  exit 1
+fi
 
-echo "Done"
+# install the ObjectBox-C library
+mkdir -p objectboxlib && cd objectboxlib
+bash <(curl -s https://raw.githubusercontent.com/objectbox/objectbox-c/master/download.sh)
+cd -
+
+if [ -x "$(command -v ldconfig)" ]; then
+  if ! ldconfig -p | grep -q "libobjectbox."; then
+    echo "installation of the C library failed - ldconfig -p doesn't report libobjectbox. Please try running again."
+    exit 1
+  fi
+fi
+
+# go get objectbox
+if [ -f "go.mod" ] && grep -q "module github.com/objectbox/objectbox-go" "go.mod"; then
+  echo "Seems like we're running inside the objectbox-go directory itself, skipping \`go get\` of the objectbox module"
+else
+  go get -u github.com/objectbox/objectbox-go/objectbox
+
+  if [[ -f "go.mod" ]]; then
+    echo "Found go.mod, skipping manual flatbuffers installation as they're already a dependency of the objectbox module"
+  fi
+fi
+
+echo "Installation complete"

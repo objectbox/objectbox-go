b4acd1a91077bb8486286fb6784994ab467c4308
Ivan Dlugos <noreply+ivan@objectbox.io>
2019-07-16 13:23:40 +0200

    install.sh - note for windows users to copy the lib to a runtime visible dir

--- install.sh
+++ install.sh
@@ -2,19 +2,20 @@
 set -eu
 
 cLibVersion=0.6.0
+os=$(uname)
 
 goVersion=$(go version | cut -d' ' -f 3)
 goVersionMajor=$(echo "${goVersion}" | cut -d'.' -f1)
 goVersionMinor=$(echo "${goVersion}" | cut -d'.' -f2)
 goVersionPatch=$(echo "${goVersion}" | cut -d'.' -f3)
 
-if [ ! "${goVersionMajor}" == "go1" ]; then
+if [[ ! "${goVersionMajor}" == "go1" ]]; then
   echo "Unexpected Go major version ${goVersionMajor}, expecting Go 1.11.4+."
   echo "You can proceed and let us know if you think we should extend the support to your version."
-elif [ "${goVersionMinor}" -lt "11" ]; then
+elif [[ "${goVersionMinor}" -lt "11" ]]; then
   echo "Invalid Go version ${goVersion}, at least 1.11.4 required."
   exit 1
-elif [ "${goVersionMinor}" == "11" ] && [ "${goVersionPatch}" -lt "4" ]; then
+elif [[ "${goVersionMinor}" == "11" ]] && [[ "${goVersionPatch}" -lt "4" ]]; then
   echo "Invalid Go version ${goVersion}, at least 1.11.4 required."
   exit 1
 fi
@@ -22,11 +23,17 @@ fi
 # install the ObjectBox-C library
 mkdir -p objectboxlib && cd objectboxlib
 bash <(curl -s https://raw.githubusercontent.com/objectbox/objectbox-c/master/download.sh) ${cLibVersion}
+if [[ "$os" == MINGW* ]] || [[ "$os" == CYGWIN* ]]; then
+    localLibDir=$(realpath lib)
+    echo "Windows must be able to find the objectbox.dll when executing ObjectBox based programs (even tests)."
+    echo "One way to accomplish that is to manually copy the objectbox.dll from ${localLibDir} to C:/Windows/System32/ folder."
+    echo "See https://golang.objectbox.io/install#objectbox-library-on-windows for more details."
+fi
 cd -
 
-if [ -x "$(command -v ldconfig)" ]; then
+if [[ -x "$(command -v ldconfig)" ]]; then
   if ! ldconfig -p | grep -q "libobjectbox."; then
-    echo "installation of the C library failed - ldconfig -p doesn't report libobjectbox. Please try running again."
+    echo "Installation of the C library failed - ldconfig -p doesn't report libobjectbox. Please try running again."
     exit 1
   fi
 fi

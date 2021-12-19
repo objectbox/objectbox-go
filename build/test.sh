eeef5a552f4e1893beb6bd55da6f9653049f0278
Ivan Dlugos <dlugos.ivan@gmail.com>
2021-12-19 14:48:42 +0100

    fix test.sh for osx grep

--- build/test.sh
+++ build/test.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-# use GNU grep when available (usually MacOS)
-if [[ -x "$(command -v ggrep)" ]]; then
-    grep=ggrep
-else
-    grep=grep
-fi
-
 unformatted_files=$(gofmt -l .)
 if [[ ${unformatted_files} ]]; then
     echo "Some files are not formatted properly. You can use \`gofmt -l -w .\` to fix them:"
@@ -17,7 +10,4 @@ fi
 
 go vet ./...
 
-# find a list of all tests in the project (go list output ending with "test]")
-tests=$(go list -test ./... | $grep -oP '\[\K.*(?=.test\])')
-
-go test "$@" $tests
+go test "$@" ./...

6f3f4ed44479283d273529f6f01124fdf97348d4
Markus <markus@greenrobot>
2023-06-23 10:59:32 +0200

    test.sh: only check gofmt if reference Go version is found

--- build/test.sh
+++ build/test.sh
@@ -5,13 +5,24 @@ set -eu
 script_dir=$( cd "$(dirname "$0")" ; pwd -P )
 cd "${script_dir}/.." # move to project root dir
 
-unformatted_files=$(gofmt -l .)
-if [[ ${unformatted_files} ]]; then
-    echo "Some files are not formatted properly. You can use \`gofmt -l -w .\` to fix them:"
-    printf "%s\n" "${unformatted_files}"
-    exit 1
+go_version=$(go version)
+echo $go_version
+if [[ $go_version == *go1.17.* ]]; then  # Keep in sync with our CI gatekeeper job; TODO update to latest
+  # gofmt is version specific, so only run this for our reference version for development
+  echo "Reference Go version found for gofmt; checking source format..."
+  echo "******** Testing: gofmt ********"
+  unformatted_files=$(gofmt -l .)
+  if [[ ${unformatted_files} ]]; then
+      echo "Some files are not formatted properly. You can use \`gofmt -l -w .\` to fix them:"
+      printf "%s\n" "${unformatted_files}"
+      exit 1
+  fi
+else
+  echo "The found Go version is not our reference for gofmt; skipping source format check"
 fi
 
+echo "******** Testing: go vet ********"
 go vet ./...
 
+echo "******** Testing: go test ********"
 go test "$@" ./...
